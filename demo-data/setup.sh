f095b42a4339782bc015c4a65977461f2f7bfdcd
Bret McGowen <bretmcg@gmail.com>
2017-07-18 05:02:15 -0400

    added setup.sh

---
+++ demo-data/setup.sh
@@ -0,0 +1,10 @@
+# Demo reset.
+kubectl delete deployment gif-maker
+cp ../Dockerfile-local ../Dockerfile
+# Reset quota file.
+
+
+# Just once.
+# kubectl expose deployment gif-maker --type=LoadBalancer --name=gif-maker-service
+
+
