377b9416d455cee8db3c0f31f49737fa1f4982c2
Bret McGowen <bretmcg@gmail.com>
2017-07-18 10:56:38 -0400

    reset context

--- demo-files/reset-demo.sh
+++ demo-files/reset-demo.sh
@@ -9,8 +9,9 @@ cp cloudbuild.v001.yaml ../cloudbuild.yaml
 kubectl delete deployments gif-maker --context dev
 kubectl delete deployments gif-maker --context bretmcg
 kubectl delete deployments gif-maker --context production
-git tag -d 0.0.1
-git push --delete origin 0.0.1
+kubectl config use-context dev
+# git tag -d 0.0.1
+# git push --delete origin 0.0.1
 
 
 # Just once per env.
